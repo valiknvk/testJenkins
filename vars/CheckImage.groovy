cfe27862214b779dae02941292893874607c01bf
Valik <48516714+valiknvk@users.noreply.github.com>
2024-10-25 19:03:29 +0300

    Update CheckImage.groovy

--- vars/CheckImage.groovy
+++ vars/CheckImage.groovy
@@ -1,4 +1,4 @@
-class DockerChanges {
+class CheckImage {
     static Boolean checkImageExists(imageName) {
   	def check = sh(script: "docker image ls | grep -w ${imageName}", returnStatus: true);
         return !check;
