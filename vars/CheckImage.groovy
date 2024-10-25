48f341adfc8f1f4f6dc11432a2c5a64412fef77c
Valik <48516714+valiknvk@users.noreply.github.com>
2024-10-25 19:18:11 +0300

    Update CheckImage.groovy

--- vars/CheckImage.groovy
+++ vars/CheckImage.groovy
@@ -2,6 +2,6 @@ class CheckImage {
     static void checkImageExists(steps) {
         String imageName = steps.env.DOCKER_IMAGE_NAME;
       	boolean check = steps.sh(script: "docker image ls | grep -w ${imageName}", returnStatus: true);
-        steps.env.IMAGE_EXISTS = !check;
+        steps.env.IMAGE_EXISTS = check;
     }
 }
