1621666f7971b62d1de3c797da0c4532e7a17ee5
Valik <48516714+valiknvk@users.noreply.github.com>
2024-10-27 16:00:55 +0300

    Update boolImageCheck.groovy

--- vars/boolImageCheck.groovy
+++ vars/boolImageCheck.groovy
@@ -1,6 +1,6 @@
 class boolImageCheck {
     static Boolean checkImageExists(steps, imageName) {
-  	    def check = steps.sh(script: "docker image ls | grep -w ${imageName}", returnStatus: true);
+  	    boolean check = steps.sh(script: "docker image ls | grep -w ${imageName}", returnStatus: true);
         return !check;
     }
 }
