8a86245fcc0eb3f50433066687b248bb58990192
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-13 12:15:18 +0300

    Update DockerChanges.groovy

--- vars/DockerChanges.groovy
+++ vars/DockerChanges.groovy
@@ -1,10 +1,10 @@
 class DockerChanges {
     String registryUrl = 'https://docker.valuesoft.site';
-    static String getRegistry() {
+    String getRegistry() {
     	return registryUrl;
     }
     static Boolean checkImageExists(steps, imageName) {
-	boolean check = steps.docker.withRegistry(${registryUrl}, 'docker') { 
+	boolean check = steps.docker.withRegistry(registryUrl, 'docker') { 
 		steps.sh(script: "docker manifest inspect docker.valuesoft.site/${imageName}", returnStatus: true);
 	}
         return !check;
