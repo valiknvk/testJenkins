06ba5b052c45488ce96f67bdc1fb6c0bf8c30e69
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 18:31:28 +0300

    Update Queue.groovy

--- vars/Queue.groovy
+++ vars/Queue.groovy
@@ -1,20 +1,11 @@
-//import jenkins.model.Jenkins;
-//import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution;
-
-//import java.util.List;
+package com.forkshunter.jenkins
+import jenkins.model.Jenkins;
+import hudson.model.Queue;
 
 class Queue {
-    static List<?> printQueue() {
-        var queue = Jenkins.get().getQueue();
-        //List<?> items = queue.getItems();
-        return queue.getItems();
-        //for (Object item : items) {
-        //    var task = item.getTask();
-        //    if (task instanceof ExecutorStepExecution.PlaceholderTask) {
-        //        System.out.println("Job name: " + task.getOwnerExecutable().getParent().getFullName());
-        //    } else {
-        //        System.out.println("Job name: " + task.getFullName());
-        //    }
-        }
-    }
+    public void cleanQueue() {
+	Queue queue = Jenkins.getInstance().getQueue();
+	queue.getItems().stream()
+        .filter(item -> item.getTask().getName().contains("booker/master"))
+        .forEach(item -> queue.cancel(item.getTask()));
 }
