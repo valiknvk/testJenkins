0af5751fe0cbd81ae27dddef2193377c93a95fba
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 13:20:43 +0300

    Update Queue.groovy

--- vars/Queue.groovy
+++ vars/Queue.groovy
@@ -1,13 +1,19 @@
-class Queue {
-  static String printQueue(){
-    def queue = Jenkins.get().queue
-	for ( def item queue.items) { 
-    	  def task = item.task 
-      	  if (task instanceof org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution.PlaceholderTask) {
-      	      System.out.println("Job name: " + task.getOwnerExecutable().getParent().getFullName())
-    	    } else {
-      	      System.out.println("Job name: " + task.getFullName())
-    	    }
-    }	
-  }
+//import jenkins.model.Jenkins;
+//import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution;
+
+//import java.util.List;
+
+public class printQueue {
+    public static void main(String[] args) {
+        var queue = Jenkins.get().getQueue();
+        List<?> items = queue.getItems();
+        for (Object item : items) {
+            var task = item.getTask();
+            if (task instanceof ExecutorStepExecution.PlaceholderTask) {
+                System.out.println("Job name: " + task.getOwnerExecutable().getParent().getFullName());
+            } else {
+                System.out.println("Job name: " + task.getFullName());
+            }
+        }
+    }
 }
