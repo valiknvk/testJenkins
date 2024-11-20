9f5b13680eaafdb8ca96d63b72ea9446f9f19e25
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 19:11:48 +0300

    Create QueueCustom.groovy

---
+++ vars/QueueCustom.groovy
@@ -0,0 +1,11 @@
+import jenkins.model.Jenkins;
+import hudson.model.Queue;
+
+class QueueCustom {
+    public void cleanQueue() {
+        Queue queue = Jenkins.getInstance().getQueue();
+        queue.getItems().stream()
+            .filter(item -> item.getTask().getName().contains("booker/master"))
+            .forEach(item -> queue.cancel(item.getTask()));
+    }
+}
