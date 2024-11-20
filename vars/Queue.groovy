12f793a14de482cf04c7a94164594bfb76529c67
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 18:42:54 +0300

    Update Queue.groovy

--- vars/Queue.groovy
+++ vars/Queue.groovy
@@ -2,7 +2,7 @@ import jenkins.model.Jenkins;
 import hudson.model.Queue;
 
 class QueueHandler {
-    void clean() {
+    static void clean() {
         Queue queue = Jenkins.getInstance().getQueue();
         queue.getItems().stream()
             .filter(item -> item.getTask().getName().contains("part"))
