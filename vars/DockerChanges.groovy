c199bd242e08cbf055a2de1947e7a0317e97a080
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 11:44:35 +0300

    Update DockerChanges.groovy

--- vars/DockerChanges.groovy
+++ vars/DockerChanges.groovy
@@ -9,4 +9,14 @@ class DockerChanges {
 	}
         return !check;
     }
+    def queue = Jenkins.get().queue
+	sh ("echo $queue")
+	queue.items.each{ item ->
+    	task = item.task 
+      	if (task instanceof org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution.PlaceholderTask) {
+      	    println("Job name: " + task.getOwnerExecutable().getParent().getFullName())
+    	        } else {
+      	    println("Job name: " + task.getFullName())
+    	    }
+        }	
 }
