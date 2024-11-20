e1022f13df07f2ec95219403fb4f46b2825677ab
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-20 18:38:43 +0300

    Update Queue.groovy

--- vars/Queue.groovy
+++ vars/Queue.groovy
@@ -1,5 +1,5 @@
 class Queue {
-	static void clean() {
+	void clean() {
     		def queue = Jenkins.instance.queue
     		queue.items.findAll { it.task.name.contains('booker/master') }.each {
 			queue.cancel(it.task) 
