d4ffba7c602fe10927869352da35f0a30b572cc1
Valik <48516714+valiknvk@users.noreply.github.com>
2024-11-21 13:00:04 +0300

    Update QueueCustom.groovy

--- vars/QueueCustom.groovy
+++ vars/QueueCustom.groovy
@@ -1,8 +1,8 @@
 import jenkins.model.Jenkins
 class QueueCustom {
-	static void cleanQueueCustom() {
+	static void cleanQueueCustom(String taskName) {
     		def queue = Jenkins.instance.queue
-    		queue.items.findAll { it.task.name.contains('booker/master') }.each {
+    		queue.items.findAll { it.task.name.contains(taskName) }.each {
 			queue.cancel(it.task) 
     		}
 	}
